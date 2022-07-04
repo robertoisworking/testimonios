80943fa9a7f0e1518173b901fef1f711f398bf34
Roberto Perez <robertoisworking@gmail.com>
2022-07-03 23:32:22 -0300

    Testimonios!

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../stylesheets/Testimonios.css';
+
+function Testimonio(props) {
+  return (
+    <div className='contenedor-testimonio'>
+      <img 
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt='Testimonio de Emma'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-pais-testimonio'><strong>{props.nombre}</strong> de {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
